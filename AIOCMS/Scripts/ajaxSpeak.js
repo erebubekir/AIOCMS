7dea83b8f6902477c29257fe86c453113f1b2860
ali ihsan sözer <aliihsansozer@hotmail.com>
2020-11-04 11:10:57 +0300

    ajax için merkez bir function belirlendi

--- AIOCMS/Scripts/ajaxSpeak.js
+++ AIOCMS/Scripts/ajaxSpeak.js
@@ -1,10 +1,10 @@
 ﻿//Bu Fonksiyon ajax işlemlerini uzun uzun yapmak yerine hızlı yapmamıza Olanak Tanıyor
 
-function ajaxSpeak(url, data, callback, method) {
-    if (url == "" || url == "undefined") {
+function ajaxSpeak(url, data, method, callback) {
+    if (url == "" || url == 'undefined') {
         url = location.href;
     }
-    if (method == "" || method == "undefined") {
+    if (method == "" || method == 'undefined') {
         method = "post";
     }
     $.ajax({
@@ -13,16 +13,27 @@ function ajaxSpeak(url, data, callback, method) {
         data: data, // gönderilcek veriler
         success: function (cevap) { // eğer başarılı ise gelen cevabı cevap değişkenine basar
             cevap = $.parseJSON(cevap);// Gelen Değeri Json Olarak Parse Ediyoruz
-            swal(cevap.message, "", cevap.status).then((value) => {
-                if (cevap.href != 'undefined') {
-                    location.href = cevap.href;
-                }
-                if (cevap.reload != 'undefined') {
-                    location.reload();
-                }
-                callback(cevap);
-            });
+            if (cevap.message != "" && cevap.message != 'undefined') {
+                swal(cevap.message, "", cevap.status).then((value) => {
+                    speakLoad(cevap);
+                    if (callback != "" && callback != 'undefined') {
+                        callback(cevap);
+                    }
+
+                });
+            } else {
+                speakLoad(cevap);
+            }
             return false;
         }
     });
+}
+
+function speakLoad(cevap) {
+    if (cevap.href != 'undefined') {
+        location.href = cevap.href;
+    }
+    if (cevap.reload != 'undefined') {
+        location.reload();
+    }
 }
